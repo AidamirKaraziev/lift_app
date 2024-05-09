e1c9f60ed67e2ea1a49f2e77f0ffbaa4eff74594
ajdamir.karaziev <ajdamir.karaziev@gmail.com>
2024-05-09 14:19:22 +0300

    fix celeryworker_pre_start.py

--- backend/app/app/celeryworker_pre_start.py
+++ backend/app/app/celeryworker_pre_start.py
@@ -1,7 +1,5 @@
 import logging
 
-from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
-
 from app.db.session import SessionLocal
 
 logging.basicConfig(level=logging.INFO)
@@ -11,12 +9,6 @@ max_tries = 60 * 5  # 5 minutes
 wait_seconds = 1
 
 
-@retry(
-    stop=stop_after_attempt(max_tries),
-    wait=wait_fixed(wait_seconds),
-    before=before_log(logger, logging.INFO),
-    after=after_log(logger, logging.WARN),
-)
 def init() -> None:
     try:
         # Try to create session to check if DB is awake
