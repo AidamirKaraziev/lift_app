6c0e3757ac752ac92a086426d194c7e92794ba2f
ajdamir.karaziev <ajdamir.karaziev@gmail.com>
2024-05-10 12:43:02 +0300

    upgrade act_fact.py models

--- backend/app/app/models/act_fact.py
+++ backend/app/app/models/act_fact.py
@@ -1,26 +1,24 @@
+from datetime import date
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
-
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
-
-
 from app.models import Object, ActBase, Status
 
 
 class ActFact(Base):
     __tablename__ = "acts_fact"
     id = Column(Integer, primary_key=True)
-    object_id = Column(Integer, ForeignKey('objects.id', ondelete="SET NULL"))
-    act_base_id = Column(Integer, ForeignKey('acts_bases.id', ondelete="SET NULL"))
+    object_id = Column(Integer, ForeignKey('objects.id', ondelete="SET NULL", onupdate="CASCADE"))
+    act_base_id = Column(Integer, ForeignKey('acts_bases.id', ondelete="SET NULL", onupdate="CASCADE"))
     step_list_fact = Column(String)
-    date_create = Column(Date)
+    date_create = Column(Date, default=date.today)
     date_start = Column(Date)
     date_finish = Column(Date)
-    foreman_id = Column(Integer, ForeignKey("universal_users.id", ondelete="SET NULL"))
-    main_mechanic_id = Column(Integer, ForeignKey("universal_users.id", ondelete="SET NULL"))
+    foreman_id = Column(Integer, ForeignKey("universal_users.id", ondelete="SET NULL", onupdate="CASCADE"))
+    main_mechanic_id = Column(Integer, ForeignKey("universal_users.id", ondelete="SET NULL", onupdate="CASCADE"))
     file = Column(String)
-    status_id = Column(Integer, ForeignKey('statuses.id', ondelete="SET NULL"))
+    status_id = Column(Integer, ForeignKey('statuses.id', ondelete="SET NULL", onupdate="CASCADE"))
 
     object = relationship(Object)
     act_base = relationship(ActBase)
@@ -28,5 +26,5 @@ class ActFact(Base):
     main_mechanic = relationship("UniversalUser", foreign_keys=[main_mechanic_id])
     status = relationship(Status)
 
-    acts_fact_of_mechanic = relationship('ActFactOfMechanic',
-                                         back_populates='act_fact', cascade="all, delete")
+    # удаляю эту строчку, потому что не понимаю зачем она
+    # acts_fact_of_mechanic = relationship('ActFactOfMechanic', back_populates='act_fact', cascade="all, delete")
