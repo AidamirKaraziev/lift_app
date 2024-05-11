2f6727b734bf0de920d52d4fe038333172dc3720
ajdamir.karaziev <ajdamir.karaziev@gmail.com>
2024-05-11 19:48:25 +0300

    upgrade crud_status.py

--- backend/app/app/crud/crud_status.py
+++ backend/app/app/crud/crud_status.py
@@ -1,18 +1,23 @@
+from starlette import status
 from sqlalchemy.orm import Session
+
 from app.crud.base import CRUDBase
 
-from app.core.templates_raise import status_not_found
 from app.models import Status
 from app.schemas.status import StatusCreate, StatusUpdate
 
 
 class CrudStatus(CRUDBase[Status, StatusCreate, StatusUpdate]):
-    not_found = status_not_found
+    obj_name = "Статусы"
+    not_found_id = {
+        "status_code": status.HTTP_404_NOT_FOUND,
+        "detail": f"{obj_name}: не найден с таким id"
+    }
 
     def getting_status(self, *, db: Session, status_id: int):
         obj = db.query(Status).filter(Status.id == status_id).first()
         if obj is None:
-            return None, self.not_found, None
+            return None, self.not_found_id, None
         return obj, 0, None
 
 
